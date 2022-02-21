b90b9ca87b1c5d4a70917131e2de45f04aae3740
Valdir Lima <valdirlima508@gmail.com>
2022-02-21 16:42:31 -0500

    initial commit

--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -17,6 +17,36 @@ namespace CarLotSimulator
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
+            Car car1 = new Car();
+            car1.Year = 2020;
+            car1.Make = "Honda";
+            car1.Model = "Accord";
+            car1.EngineNoise = "VROOM";
+            car1.HonkNoise = "HONK HONK";
+            car1.IsDriveable = true;
+            car1.MakeHonkNoise(car1.HonkNoise);
+            car1.MakeEngineNoise(car1.EngineNoise);
+
+            var car2 = new Car();
+            car2.Year = 2000;
+            car2.Make = "Ford";
+            car2.Model = "Mustang";
+            car2.EngineNoise = "VROOM VROOM";
+            car2.HonkNoise = "HONK";
+            car2.IsDriveable = true;
+            car2.MakeHonkNoise(car2.HonkNoise);
+            car2.MakeEngineNoise(car2.EngineNoise);
+
+            var car3 = new Car();
+            car3.Year = 2020;
+            car3.Make = "Chevy";
+            car3.Model = "Tahoe";
+            car3.EngineNoise = "Brroooom";
+            car3.HonkNoise = "Beep Beep";
+            car3.IsDriveable = true;
+            car3.MakeHonkNoise(car3.HonkNoise);
+            car3.MakeEngineNoise(car3.EngineNoise);
+
 
             //*************BONUS*************//
 
